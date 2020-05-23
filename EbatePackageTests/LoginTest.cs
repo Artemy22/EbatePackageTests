4ecdbffdba8b35fb6784d1a450b2aec72cca649c
Artemy22 <artem.nii@gmail.com>
2020-05-23 11:07:13 +0300

    added: loginPObj, loginTenPObj, testLogin

---
+++ EbatePackageTests/LoginTest.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace EbatePackageTests
+{
+    public class LoginTest
+    {
+        private IWebDriver driver;
+        private readonly Credentials creds = new Credentials();
+      
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://app.test.e-bate.net/login");
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            var loginPage = new LoginTabPageObject(driver);
+            var loginTenantnPage = new LoginTenantTabPageObject(driver);
+
+            loginPage.Login(creds.Email, creds.Password);
+            Thread.Sleep(1000);
+            loginTenantnPage.ChooseFirstTenant();
+
+            Assert.Pass();
+        }
+    }
+}
